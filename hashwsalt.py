97f0d06a5d2d60bc4bf58098b3782675d4e2a772
Divvij Chandna <divvijchandna@gmail.com>
2021-11-07 17:58:22 +0800

    Update hashwsalt.py

--- hashwsalt.py
+++ hashwsalt.py
@@ -6,29 +6,53 @@ import ssl
 
 # password = 'password123'
 
-def get_hashed_password(password):
+def get_vault_key(master_password, username):
 
     salt = os.urandom(32) # Remember this
+    combine = username + master_password
 
     key = hashlib.pbkdf2_hmac(
         'sha256', # The hash digest algorithm for HMAC
-        password.encode('utf-8'), # Convert the password to bytes
+        combine.encode('utf-8'), # Convert the password to bytes
+        salt, # Provide the salt
+        100000, # It is recommended to use at least 100,000 iterations of SHA-256 
+        256
+    )
+
+    vault_key = salt + key
+    return vault_key
+
+def get_auth_hash(vault_key, salt, master_password):
+    
+    combine = str(vault_key) + master_password
+
+    key = hashlib.pbkdf2_hmac(
+        'sha256', # The hash digest algorithm for HMAC
+        combine.encode('utf-8'), # Convert the password to bytes
         salt, # Provide the salt
         100000 # It is recommended to use at least 100,000 iterations of SHA-256 
     )
 
-    hash_storage = salt + key
-    return hash_storage
+    auth_hash = salt + key
+    return auth_hash
 
-def check_password(password_to_check, hash_storage):
+def check_password(password_to_check, username, auth_hash):
 
-    salt_from_storage = hash_storage[:32] # 32 is the length of the salt
-    key_from_storage = hash_storage[32:]
+    salt_from_storage = auth_hash[:32] # 32 is the length of the salt
+    key_from_storage = auth_hash[32:]
 
     # Use the exact same setup you used to generate the key, but this time put in the password to check
+    vault_key = hashlib.pbkdf2_hmac(
+        'sha256',
+        (username + password_to_check).encode('utf-8'), # Convert the password to bytes
+        salt_from_storage, 
+        100000,
+        256
+    )
+
     new_key = hashlib.pbkdf2_hmac(
         'sha256',
-        password_to_check.encode('utf-8'), # Convert the password to bytes
+        (str(vault_key) + password_to_check).encode('utf-8'), # Convert the password to bytes
         salt_from_storage, 
         100000
     )
@@ -39,7 +63,7 @@ def check_password(password_to_check, hash_storage):
         print('Password is incorrect')
 
 
-def store_password(username, hash_storage):
+def store_auth_hash(username, auth_hash):
 
     uri = "mongodb+srv://divvij:.cabbDvp9V7tTy8@cluster0.tz0g1.mongodb.net/passwordManager?retryWrites=true&w=majority"
     client = MongoClient(uri, ssl_cert_reqs=ssl.CERT_NONE)
@@ -47,7 +71,7 @@ def store_password(username, hash_storage):
     db = client.passwordManager
     collection = db.masterPasswords
 
-    post = {"username": username, "password_hash": hash_storage}
+    post = {"username": username, "auth_hash": auth_hash}
 
     collection.insert_one(post)
 
@@ -61,7 +85,7 @@ def retrieve_password(username):
 
     result = collection.find({"username": username})
 
-    return result[0]["password_hash"]
+    return result[0]["auth_hash"]
 
 option = input("Add or Check (a/c)? ")
 
@@ -70,19 +94,21 @@ if option == 'a':
     username = input("Username: ")
     password = getpass()
 
-    hash_storage = get_hashed_password(password)
+    vault_key = get_vault_key(password, username)
+    salt = vault_key[:32] # 32 is the length of the salt
+    vault_key = vault_key[32:]
+    auth_hash = get_auth_hash(vault_key, salt, password)
 
-    store_password(username, hash_storage)
+    store_auth_hash(username, auth_hash)
 
 elif option == 'c':
 
     username = input("Username: ")
     password = getpass()
 
-    hash_storage = retrieve_password(username)
-    print(hash_storage)
+    auth_hash = retrieve_password(username)
 
-    check_password(password, hash_storage)
+    check_password(password, username, auth_hash)
 
 
 
