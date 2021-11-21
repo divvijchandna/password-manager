2438cbf679a3ac8a5e2db6386226e42bd0249d8e
AryamanShaan <aryaman.shaan@gmail.com>
2021-11-21 11:24:22 +0800

    Update password_maker.py

--- password_maker.py
+++ password_maker.py
@@ -1,6 +1,41 @@
-from Crypto import Random
+# Code based on: https://www.geeksforgeeks.org/generating-strong-password-using-python/
+from Crypto import Random  # This one giving error for now
+import random #Maybe change random package to better package
+import array
 
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
 
-DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+lower_case = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
+                     'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                     'z']
+  
+upper_case = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
+                     'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
+                     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                     'Z']
 
-print(Random)
+symbols = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>', 
+           '*', '(', ')', '<']
+
+combined = digits + lower_case + upper_case + symbols
+
+random_digit = random.choice(digits)
+random_lower = random.choice(lower_case)
+random_upper = random.choice(upper_case)
+random_symbol = random.choice(symbols)
+
+password_len = int(input('enter password length: '))
+
+tmp_password = random_digit + random_lower + random_upper + random_symbol
+
+for i in range (0, password_len-4):
+    tmp_password = tmp_password + random.choice(combined)
+    temp_pass_list = array.array('u', tmp_password)
+    random.shuffle(temp_pass_list)
+
+password = ""
+for x in temp_pass_list:
+        password = password + x
+
+print('Password is: ', password)
