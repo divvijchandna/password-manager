319da8398dc39b173d4fcb454305e58ce6e8b5f3
AryamanShaan <aryaman.shaan@gmail.com>
2021-11-28 17:28:55 +0800

    Update rough.py

--- rough.py
+++ rough.py
@@ -1,4 +1,25 @@
 record = 'instagram||divvij.chandna@gmail.com||:8KG0Shf|||facebook||divvij@ntu.com||&4yRbk'
 
+websites = []
+emails = []
+passwords = []
 
-print(record.split('|||'))
+website = 'facebOK'
+email = 'divvij@NTu.com'
+
+
+for tup in record.split('|||'):
+    li = str(tup).split('||')
+    websites.append(str(li[0]).lower())
+    emails.append(str(li[1]).lower())
+    passwords.append(str(li[2]))
+
+print(websites)
+print()
+print(emails)
+print()
+print(passwords)
+print()
+
+print(websites.index(website.lower()))
+print(emails.index(email.lower()))
